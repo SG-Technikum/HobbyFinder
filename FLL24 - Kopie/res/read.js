8e9bf10c36db8d3a35bc8ea50758a55e9fca5224
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-01-28 18:49:01 +0100

    j

--- FLL24 - Kopie/res/read.js
+++ FLL24 - Kopie/res/read.js
@@ -1,31 +1,33 @@
 let vereine = []
 class verein{
-    constructor(id, tags, description, email, location){
+    constructor(id, tags, description, email, loc){
         this.id = id
         this.tags = tags
         this.description = description
         this.email = email
-        this.location = location
+        this.loc = location
         vereine.push(this)
     }
 }
-
+//weil json nicht geht
 const Fischerclub = new verein(id="Freddys Fischerclub",
     tags=["teamwork", "fischen"],
     description="Freddys club in dem man mit freunden fischt, mindestalter 18",
     email="<EMAIL>",
-    location="Freiburg")
+    loc="Freiburg")
 const Schachverein = new verein(
     id="Sallys supertoller Schachverein",
     tags=["schach", "spiel"],
     description="text",
     email="<EMAIL>",
-    location="Bielefeld")
+    loc="Bielefeld")
 const TechnikumsAG = new verein(
     id="Technikums AG",
     tags=["technik", "teamwork"],
     description="was man halt so mit technik macht",
     email="<EMAIL>",
-    location="Brastelburg b. Lauchheim"
+    loc="Brastelburg b. Lauchheim"
 )
+
+//create one div for every verein here
 
