9c3165171719e663640530d7250a6e23061cce24
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-01-28 18:40:12 +0100

    jez gehts los!

--- FLL24 - Kopie/res/read.js
+++ FLL24 - Kopie/res/read.js
@@ -1,4 +1,31 @@
-let json = "vereine" : "json";
-let reader = new FileReader();
-let result = JSON.parse(json);
-console.log(reader.result);
+let vereine = []
+class verein{
+    constructor(id, tags, description, email, location){
+        this.id = id
+        this.tags = tags
+        this.description = description
+        this.email = email
+        this.location = location
+        vereine.push(this)
+    }
+}
+
+const Fischerclub = new verein(id="Freddys Fischerclub",
+    tags=["teamwork", "fischen"],
+    description="Freddys club in dem man mit freunden fischt, mindestalter 18",
+    email="<EMAIL>",
+    location="Freiburg")
+const Schachverein = new verein(
+    id="Sallys supertoller Schachverein",
+    tags=["schach", "spiel"],
+    description="text",
+    email="<EMAIL>",
+    location="Bielefeld")
+const TechnikumsAG = new verein(
+    id="Technikums AG",
+    tags=["technik", "teamwork"],
+    description="was man halt so mit technik macht",
+    email="<EMAIL>",
+    location="Brastelburg b. Lauchheim"
+)
+
