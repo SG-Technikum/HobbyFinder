886e496b640bd52368002ddb3174b66075641d99
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-02-02 18:07:30 +0100

    bug fixes und auch mal was neues

--- FLL24 - Kopie/res/read.js
+++ FLL24 - Kopie/res/read.js
@@ -16,18 +16,12 @@ const Fischerclub = new verein(id="Freddys Fischerclub",
     description="Freddys club in dem man mit freunden fischt, mindestalter 18",
     email="beispiel.beispiel@beispiel.com",
     loca="Freiburg")
-const Schachverein = new verein(
-    id="Sallys supertoller Schachverein",
-    tags=["schach", "spiel"],
-    description="text",
-    email="beispiel.beispiel@beispiel.com",
-    loca="Bielefeld")
 const TechnikumsAG = new verein(
     id="Technikums AG",
     tags=["technik", "teamwork"],
     description="was man halt so mit technik macht",
     email="beispiel.beispiel@beispiel.com",
-    loca="Brastelburg b. Lauchheim"
+    loca="Aalen, Rombacher Straße"
 )
 const AnglerVerein = new verein
     (id="Manu's Anglerverein",
@@ -35,13 +29,6 @@ const AnglerVerein = new verein
     description="Angeln mit Manuel W.",
     email="info@angelclub.com",
     loca="Edersee, an der Kurve")
-
-const Baschtelklub = new verein(
-    id="Bobs Brilliander Baschdlklubb",
-    tags=["basteln", "spiel", "spaß"],
-    description="Hier wird viel gebastelt und experimentiert!!",
-    email="teilnehmen@basteln.de",
-    loca="Bastelbrugg")
 const Fluchschulungsvereinstrainingsprofessorausbildungsveranstaltungsorganisationsunternehmensveranstaltung = new verein(
     id="Flugschule",
     tags=["fliegen", "landen"],
@@ -49,6 +36,56 @@ const Fluchschulungsvereinstrainingsprofessorausbildungsveranstaltungsorganisati
     email="Flieger@böing.xyz",
     loca="N 6th St, Renton, WA 98057, USA"
 )
+const Internetcafe = new verein(id="Igors Internet-Cafe Experimental (ICE)",
+    tags=["Internet", "Kaffe", "Computer", "Kaffee Maschine bedienen"],
+    description="Igors Internet Café!programmier mit Freunden und trink dabei Kaffee, Mindestalter 14",
+    email="Trink.Cafe@gmail.com",
+    loca="Zella-Mehlis")
+const Judogruppe = new verein(id="Johannes Judogruppe",
+    tags=["Kämpfen", "Judo", "Turniere", "Fallen"],
+    description="Johannes Judogruppe. Lernen wie man sich verteidigt und andere Leute zu Fall bringen kann, mindestalter 16",
+    email="jojo@judo.com",
+    loca="Alexanderplatz, Berlin")
+const Jodelclub = new verein(id="Judiths Jodelclub",
+    tags=["Jodeln", "Mit Freuden", "Singen", "Laut"],
+    description="Judiths Jodelclub, Jodel Laut mit Freude und Freunden, du kannst dafür auch ein Jodeldiplom erhalten!",
+    email="jodeldiplom@gmail.com",
+    loca="Kempten im Allgäu")
+const Jachtenclub = new verein(id="Jonathans junior Jachtenclub",
+    tags=["Jachten", "Wasser", "Schiffe", "Geld"],
+    description="Jonathans Junior Jachtenclub. Jachten fahren, Geld haben, Mindestlohn = VIEL",
+    email="Geldhaben.toll@gmail.com",
+    loca="Hamburg")
+const Fussball= new verein(id="FC Felipe",
+    tags=["Fussball", "Sport", "Teamwork"],
+    description="FC Felipe fördert Teamwork mithilfe von Fussball. Besser als der Sportverein von nebenan.",
+    email="sportskanone@gmail.com",
+    loca="München")
+const Tanztrupp = new verein(id="Tims Toller Tanztrupp",
+    tags=["Tanzen", "Musik"],
+    description="Tims Toller Tanztrupp. Tanzen mit treuen Freunden und Tim",
+    email="TanzenMitTim@gmail.com",
+    loca="Iggingen")
+const Lesegruppe = new verein(id="Lennarts Lesegruppe",
+    tags=["Lesen", "Bücher", "Geschichten"],
+    description="Lennarts Lesegruppe, Lesen mit Spaß, Alter 7-12",
+    email="Lesen.Ja@gmail.com",
+    loca="Lauchheim")
+const Maler = new verein(id="Moritz' Maler",
+    tags=["Malen", "Bilder"],
+    description="Moritz' Maler, Malen anderer Leute, Objekte und sogar anderer Bilder",
+    email="moritz.maler@gmail.com",
+    loca="Oberkochen")
+const ProgrammierVerein = new verein(id="Valentins Programmier Verein",
+    tags=["Programmieren", "Computer", "Python", "Technik"],
+    description="Valentins Programmier Verein fördert den Umgang mit Computern, Mindestalter 8",
+    email="Programmier.Geil@web.com",
+    loca="Aalen")
+const KletterKinder = new verein(id="Miriams Kletter Kinder",
+    tags=["Klettern", "Kletterwand", "Bouldern"],
+    description="Miriams Kletter Kinder fördert Kinder beim Klettern, Mindestalter 8, Sturzresistenz wünschenswert.",
+    email="Kletter.Kinder@gmail.com",
+    loca="Kleinkleba bei Dresden")
 //create one div for every verein here
 
 for (let i = 0; i < vereine.length; i++){
